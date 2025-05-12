6d3beeaf199dc8dba4999160af72a56612f9e5e1
Ariel Caplan <ariel.caplan@shopify.com>
2025-05-12 16:21:30 +0300

    Make all error details properties optional

--- packages/app/src/cli/utilities/developer-platform-client.ts
+++ packages/app/src/cli/utilities/developer-platform-client.ts
@@ -234,9 +234,10 @@ export interface UserError {
 }
 
 interface ErrorDetail {
-  extension_id: number | string
+  extension_id?: number | string
   extension_title?: string
   specification_identifier?: string
+  [key: string]: unknown
 }
 
 export interface DeveloperPlatformClient {
